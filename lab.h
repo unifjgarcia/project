44bc106b4f5ae3c79c0e74e6faffd198bdaf8aee
unifjgarcia <149002145+unifjgarcia@users.noreply.github.com>
2023-10-29 17:16:29 -0300

    Update lab.h

--- lab.h
+++ lab.h
@@ -1,1 +1,26 @@
+#ifndef PROJETO_LAB_H
+#define PROJETO_LAB_H
 
+//Definindo número máximo de clientes e operações
+#define MAX_CLIENTES 1000
+#define MAX_OPERACOES 100
+
+//Representação para os tipos de conta do banco
+typedef enum {
+    comum,
+    plus
+} TipoConta;
+
+//Representação dos dados de um cliente
+typedef struct {
+    char nome[50];
+    char cpf[12];
+    TipoConta tipoConta;
+    float saldo;
+    char senha[20];
+    int operacoes[MAX_OPERACOES];
+} Cliente;
+
+int my_strcmp(const char *str1, const char *str2);
+
+#endif
